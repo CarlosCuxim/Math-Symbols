7fe7638a0666157ba2fbb169312e853d930d218e
Carlos Cuxim <carlos.cuxim@gmail.com>
2021-05-25 19:57:06 -0500

    Agregado corrección menor

--- MathSymbols.py
+++ MathSymbols.py
@@ -20,32 +20,41 @@ superscript = JSONtoDict("Symbols/superscript.json")
 
 
 def getFirstNonLetterIndex(string, begin=0):
-    string = string.lower()
     Letters = "abcdefghijklmnopqrstuvwxyz"
-
+    # Conversión de strings a minúsculas
+    string = string.lower()
+    # Busqueda del primer carácter no letra
     for i in range(begin,len(string)):
         if string[i] not in Letters:
             return i
 
 def replaceFirstToken(string, active="\\", dictionary=symbols):
+    # Obtención de index del inicio y fin del token
     bi = string.index(active)
     ei = getFirstNonLetterIndex(string, bi+1)
-
+    # Partición del string de antes y despues del token
     begin = string[:bi]
     token = string[bi+1:ei]
     end = string[ei:] if ei else ''
-
+    # Remplazo del token usando el diccionario
     if token in dictionary:
         token = symbols[token]
+    else:
+        token = active + token
+
     return begin + token + end
 
 def replaceTokens(string, active="\\", dictionary=symbols):
-    while active in string:
-        string = replaceFirstToken(string, active, dictionary)
+    # Busqueda del tokens por cada 
+    for i in range(len(string)):
+        subString = string[i:]
+        if active in subString:
+            subString = replaceFirstToken(subString, active, dictionary)
+        string = string[:i] + subString
     return string
 
 
-string = "Hola mundo sea x \\in B y y = \\int x dx, entonces"
+string = r"Hola mundo sea x \in B y y = \int x dx, entonces \hola"
 
 A = replaceTokens(string, "\\")
 print(A)
